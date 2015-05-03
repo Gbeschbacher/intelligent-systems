73fe3476354348614bff24f52781c476198387ea
Georg Eschbacher <gbeschbacher@sbg.at>
2015-05-03 15:45:49 +0200

    results of task4.1

--- Task4/Task4.m
+++ Task4/Task4.m
@@ -1,9 +1,11 @@
 function [] = Task4()
 dA = importdata('Clustering_features/features_A.txt');
 dA_classes = importdata('Clustering_classes/classes_A.txt');
-dA_STR_LABELS = [cellstr('Cancer'), 'No Cancer');
+dA_STR_LABELS = [cellstr('Cancer'), 'No Cancer'];
+
 dB = importdata('Clustering_features/features_B.txt');
 dB_classes = importdata('Clustering_classes/classes_B.txt');
+
 dC = importdata('Clustering_features/features_C.txt');
 dC_classes = importdata('Clustering_classes/classes_C.txt');
 dC_STR_LABELS = [cellstr('co'), 'fo', 'ja', 'bl', 'rn','he', 'al', 'pu','ra', 'el', 're', 'ro','po', 'cl'];
@@ -23,21 +25,21 @@ GPLOT(Y_dC_TSNE, dC_classes,dC_STR_LABELS,'DC TSNE');
 PERPLEXITY = 5;
 Y_dB_PCA = pca(dB, 2);
 Y_dB_SAMMON = sammon(dB, 2);
-Y_dB_SNE = sne(dB, 2, PERPLEXITY);
+Y_dB_SNE = sne(dB, 2, PERPLEXITY + 70);
 Y_dB_TSNE = tsne(dB, [], 2, [], PERPLEXITY);
 
-GPLOT(Y_dB_PCA, dB_classes, [],'DC PCA');
-GPLOT(Y_dB_SAMMON, dB_classes,[],'DC SAMMON');
-GPLOT(Y_dB_SNE, dB_classes,[],'DC SNE');
-GPLOT(Y_dB_TSNE, dB_classes,[],'DC TSNE');
+GPLOT(Y_dB_PCA, dB_classes, [],'DB PCA');
+GPLOT(Y_dB_SAMMON, dB_classes,[],'DB SAMMON');
+GPLOT(Y_dB_SNE, dB_classes,[],'DB SNE');
+GPLOT(Y_dB_TSNE, dB_classes,[],'DB TSNE');
 
-PERPLEXITY = 5;
+PERPLEXITY = 15;
 Y_dA_PCA = pca(dA, 2);
 Y_dA_SAMMON = sammon(dA, 2);
 Y_dA_SNE = sne(dA, 2, PERPLEXITY);
-Y_dA_TSNE = tsne(dA, [], 2, [], PERPLEXITY);
+Y_dA_TSNE = tsne(dA, [], 2, [], 18);
 
-GPLOT(Y_dA_PCA, dB_classes, dA_STR_LABELS,'DC PCA');
-GPLOT(Y_dA_SAMMON, dB_classes,dA_STR_LABELS,'DC SAMMON');
-GPLOT(Y_dA_SNE, dB_classes,dA_STR_LABELS,'DC SNE');
-GPLOT(Y_dA_TSNE, dB_classes,dA_STR_LABELS,'DC TSNE');
+GPLOT(Y_dA_PCA, dA_classes, dA_STR_LABELS,'DA PCA');
+GPLOT(Y_dA_SAMMON, dA_classes,dA_STR_LABELS,'DA SAMMON');
+GPLOT(Y_dA_SNE, dA_classes,dA_STR_LABELS,'DA SNE');
+GPLOT(Y_dA_TSNE, dA_classes,dA_STR_LABELS,'DA TSNE');
